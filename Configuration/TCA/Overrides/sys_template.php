4a103af3fc53879e843eb15fbae28de1deb2856d
David Steeb <david.steeb@b13.de>
2017-05-18 12:02:56 +0200

    [TASK] Update TCA Configuration for V8
    Using EXT:seo_basics with TYPO3 version 8 causes deprecation warnings
    and TCA migration warnings in install tool.
    
    This change
    - moves the TCA configuration to Configuration/TCA folder
    - adds ‘renderType’ to config of ‘select’-fields
    - moves call of addStaticFile to TCA/Overrides/sys_template.php
    
    to remove the warnings and messages.

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+
+// Adding the static template for new TypoScript
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('seo_basics', 'Configuration/TypoScript', 'Metatags and XML Sitemap');
